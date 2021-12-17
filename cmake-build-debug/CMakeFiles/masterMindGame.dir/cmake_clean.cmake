0e37ef0555d60646e24021bd93c586e8d71a462c
Ehsan.M <ehsan.xbox330@gmail.com>
2021-12-17 17:05:51 +0330

    first

---
+++ cmake-build-debug/CMakeFiles/masterMindGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/masterMindGame.dir/main.cpp.obj"
+  "libmasterMindGame.dll.a"
+  "masterMindGame.exe"
+  "masterMindGame.exe.manifest"
+  "masterMindGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/masterMindGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
